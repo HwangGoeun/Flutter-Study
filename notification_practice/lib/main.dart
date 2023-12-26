1ae3c89999e38d19b7f726720959cf2b203a1110
HwangGoeun <hwang_gim@naver.com>
2023-12-26 12:58:34 +0900

    알림창 연습

--- notification_practice/lib/main.dart
+++ notification_practice/lib/main.dart
@@ -1,44 +1,14 @@
-import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  void showNotificationWithButton() {
-    AwesomeNotifications().createNotification(
-      content: NotificationContent(
-        id: 10,
-        channelKey: 'basic_channel',
-        title: 'Simple Notification',
-        body: 'Simple body',
-      ),
-      actionButtons: <NotificationActionButton>[
-        NotificationActionButton(key: 'yes', label: 'Yes'),
-        NotificationActionButton(key: 'no', label: 'No'),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          // 플로팅 버튼 생성
-          onPressed: showNotificationWithButton,
-          child: const Icon(Icons.notifications),
-        ),
-        appBar: AppBar(
-          title: const Text('Awsome notification practice'),
-        ),
-        body: const Center(
-          child: Text('Press the floating action button'),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
---
+++ timer_notification_practice/android/app/src/main/kotlin/com/example/timer_notification_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.timer_notification_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
